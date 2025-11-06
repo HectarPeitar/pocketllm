641159f2a9e49e026f4cff62b23b08487ae2c9c3
HectarPeitar <ricogroenleer@gmail.com>
2025-11-06 10:57:11 +0400

    String is passable

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os
+import sys
+from google import genai
 from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
-from google import genai
-
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+def main():
+    if len(sys.argv) < 2 or not sys.argv[1]:
+        print("Error: Please enter a string", file=sys.stderr)
+        sys.exit(1) 
+        
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=sys.argv[1]
+    )
+        
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if __name__ == "__main__":
+    main()
