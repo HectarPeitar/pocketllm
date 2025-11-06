780263535c604a127d0a51559cb61b35f6589aac
HectarPeitar <ricogroenleer@gmail.com>
2025-11-06 21:57:11 +0400

    add calculator

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,14 +12,22 @@ def main():
     if len(sys.argv) < 2 or not sys.argv[1]:
         print("Error: Please enter a string", file=sys.stderr)
         sys.exit(1) 
-        
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
+
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=sys.argv[1]
+        model="gemini-2.0-flash-001", 
+        contents=messages,
     )
         
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if "--verbose" in sys.argv[2:]:
+        print(f"User prompt: {sys.argv[1]}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
