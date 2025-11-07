d92c11ea0a3dc0aa5a20e584aad2923fc8aaa6d0
HectarPeitar <ricogroenleer@gmail.com>
2025-11-07 16:22:25 +0400

    file info function added

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,2 +1,24 @@
+import os
+from DIRNAME.FILENAME import FUNCTION_NAME
+
 def get_files_info(working_directory, directory="."):
+    full_path = os.path.join(working_directory, directory)
+    full_path = os.path.abspath(full_path)
+    working_directory = os.path.abspath(working_directory)
+
+    # Check if full_path is inside working_directory
+    if os.path.commonpath([full_path, working_directory]) != working_directory:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    # Check if full_path is a directory
+    if not os.path.isdir(full_path):
+        return f'Error: "{directory}" is not a directory'
     
+    files = []
+
+    for file in os.listdir(full_path):
+        entry_path = os.path.join(full_path, file)
+        size = os.path.getsize(entry_path)
+        is_dir = os.path.isdir(entry_path)
+        files.append(f"- {file}: file_size={size}, is_dir={is_dir}")
+    return "\n".join(files)
--- main.py
+++ main.py
@@ -8,26 +8,28 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+
 def main():
     if len(sys.argv) < 2 or not sys.argv[1]:
         print("Error: Please enter a string", file=sys.stderr)
-        sys.exit(1) 
+        sys.exit(1)
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
     ]
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", 
+        model="gemini-2.0-flash-001",
         contents=messages,
     )
-        
+
     print(response.text)
 
     if "--verbose" in sys.argv[2:]:
         print(f"User prompt: {sys.argv[1]}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()
