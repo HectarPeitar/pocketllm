a497ab45602574dc7b953f3e6a658841fea3a238
HectarPeitar <ricogroenleer@gmail.com>
2026-04-06 16:57:35 +0400

    Get file function added

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,24 +1,26 @@
 import os
-from DIRNAME.FILENAME import FUNCTION_NAME
 
 def get_files_info(working_directory, directory="."):
-    full_path = os.path.join(working_directory, directory)
-    full_path = os.path.abspath(full_path)
-    working_directory = os.path.abspath(working_directory)
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
 
     # Check if full_path is inside working_directory
-    if os.path.commonpath([full_path, working_directory]) != working_directory:
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+    if not valid_target_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
     # Check if full_path is a directory
-    if not os.path.isdir(full_path):
+    if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
     
-    files = []
-
-    for file in os.listdir(full_path):
-        entry_path = os.path.join(full_path, file)
-        size = os.path.getsize(entry_path)
-        is_dir = os.path.isdir(entry_path)
-        files.append(f"- {file}: file_size={size}, is_dir={is_dir}")
-    return "\n".join(files)
+    try:
+        files = []
+        for file in os.listdir(target_dir):
+            entry_path = os.path.join(target_dir, file)
+            size = os.path.getsize(entry_path)
+            is_dir = os.path.isdir(entry_path)
+            files.append(f"- {file}: file_size={size}, is_dir={is_dir}")
+        return "\n".join(files)
+    except Exception as e:
+        return f'Error: {e}'
